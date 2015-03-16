fd92f2e2fd02ffec64e72ac575bc4f19a249f374
4johndoe <bogdandemchenko@gmail.com>
2015-03-16 16:40:11 +0200

    trivial tkinter prg containing only a Quit button

--- main.py
+++ main.py
@@ -1,39 +1,17 @@
-import random
-from tkinter import *
+import tkinter as tk
 
-n = random.randrange(100)
 
-class MyApp:
-    def __init__(self, parent):
-        self.myParent = parent
-        self.myContainer1 = Frame(parent)
-        self.myContainer1.pack()
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
 
-        button_name = "OK"
-        self.button1 = Button(self.myContainer1,
-                              command=self.buttonHandler(button_name, 1, "Good stuff!"))
-        self.button1.configure(text=button_name, background="green")
-        self.button1.pack(side=LEFT)
-        self.button1.focus_force()
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text="Quit",
+                                    command=self.quit)
+        self.quitButton.grid()
 
-        button_name = "Cancel"
-        self.button2 = Button(self.myContainer1,
-                              command=self.buttonHandler(button_name, 2, "Bad stuff!"))
-        self.button2.configure(text=button_name, background="red")
-        self.button2.pack(side=LEFT)
-
-    def buttonHandler(self, arg1, arg2, arg3):
-        print("     buttonHandler routine received arguments:", arg1.ljust(8), arg2, arg3)
-
-        def buttonHandler_a(self, event, arg1, arg2, arg3):
-            print("buttonHandler_a received event", event)
-            self.buttonHandler(arg1, arg2, arg3)
-
-print("\n"*100)
-# clear the screen
-print("Starting program", n)
-root = Tk()
-myapp = MyApp(root)
-print("Ready to start   executing the event loop.")
-root.mainloop()
-print("Finished         executing the event loop.")
+app = Application()
+app.master.title("Sample application")
+app.mainloop()

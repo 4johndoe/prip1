2a6538bc1b27df2146dc20bdefd04359c0264e34
4johndoe <bogdandemchenko@gmail.com>
2015-03-17 15:01:42 +0200

    access to DBF(dbfread) and writedown to WORD

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-import tkinter as tk
+from dbfread import DBF
+from docx import Document
+
+db_table = DBF('test.dbf')
+ro = len(db_table)
+co = db_table.field_names.__len__()
+
+doc = Document()
+
+table = doc.add_table(rows=1, cols=co)
+header_cells = table.rows[0].cells
+header_cells[0].text = 'Number'
+header_cells[1].text = 'Name'
+header_cells[2].text = 'Date'
+header_cells[3].text = 'Show'
+
+for item in db_table:
+    cells = table.add_row().cells
+    cells[0].text = str(item['NOM'])
+    cells[1].text = str(item['NAME'])
+    cells[2].text = str(item['BIRTHDAY'])
+    cells[3].text = str(item['SHOW'])
+
+# for rec in DBF('test.dbf'):
+#     print rec['NOM'], rec['NAME'], rec['BIRTHDAY'], rec['SHOW']
 
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid()
-        self.createWidgets()
-
-    def createWidgets(self):
-        self.quitButton = tk.Button(self, text="Quit",
-                                    command=self.quit)
-        self.quitButton.grid()
-
-app = Application()
-app.master.title("Sample application")
-app.mainloop()
+doc.save('demo.docx')
